046c40668c8b78b91021790a26b12e91a6c0e528
True Code Masters <hlymca84@gmail.com>
2021-04-19 17:19:37 +0530

    Error resolved

--- src/components/DCandidateForm.js
+++ src/components/DCandidateForm.js
@@ -1,7 +1,8 @@
 import { Grid, TextField } from "@material-ui/core";
-import React from "react";
+import React,{useState} from "react";
 
 const DCandidateForm = (props) => {
+    
     return (
     <form autoComplete="off" noValidate>
         <Grid Container>
